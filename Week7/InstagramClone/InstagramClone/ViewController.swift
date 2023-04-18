8c529d9ab9ff8e9eea915b226a2c83fdb84c5809
Emine Sinem <emine@Emines-MacBook-Pro.local>
2023-04-18 13:21:58 +0300

    Start Instagram Clone

---
+++ Week7/InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Emine Sinem on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
